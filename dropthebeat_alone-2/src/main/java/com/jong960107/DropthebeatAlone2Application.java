fde2d09610b976f26c29d11d84e6a8bbb3619c49
jong960107 <jong960107@naver.com>
2022-05-02 05:13:28 +0900

    1day

---
+++ dropthebeat_alone-2/src/main/java/com/jong960107/DropthebeatAlone2Application.java
@@ -0,0 +1,13 @@
+package com.jong960107;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropthebeatAlone2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropthebeatAlone2Application.class, args);
+	}
+
+}
